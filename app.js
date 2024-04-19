f446f4b495e43a60cfc3e837fea52d7ae84881bb
Shantanu(CometChat) <93969013+Shantanu-CometChat@users.noreply.github.com>
2024-04-19 10:39:06 +0530

    added old code

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const axios = require('axios');
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
 
 const token = 'ab';
 const user = 'Shantanu-CometChat';
 const repo = 'testingcommit';
 
+
 app.use(bodyParser.json());
 
 const secret = 'YOUR_CLOUDSMITH_SECRET';
 
-app.post('/api/webhook', async (req, res) => {
+// This middleware validates the incoming webhook
+app.use((req, res, next) => {
+  next();
+});
+
+app.post('/webhook', async (req, res) => {
+
 
     console.log('Started post webhook')
 
@@ -42,4 +48,60 @@ app.post('/api/webhook', async (req, res) => {
     }
   });
 
-module.exports = app;
+
+app.listen(3000, () => console.log('Server started'));
+
+
+
+
+
+
+
+
+
+
+// const axios = require('axios');
+// const express = require('express');
+// const bodyParser = require('body-parser');
+
+// const app = express();
+
+// const token = 'ab';
+// const user = 'Shantanu-CometChat';
+// const repo = 'testingcommit';
+
+// app.use(bodyParser.json());
+
+// const secret = 'YOUR_CLOUDSMITH_SECRET';
+
+// app.post('/api/webhook', async (req, res) => {
+
+//     console.log('Started post webhook')
+
+//     try {
+//       const { data: { content, sha } } = await axios.get('https://api.github.com/repos/Shantanu-CometChat/testingcommit/contents/README.md', {
+//         headers: {
+//           'Authorization': `token ${token}`,
+//         },
+//       });
+
+//       const newContent = Buffer.from(`${Buffer.from(content, 'base64').toString('utf-8')}\nhello\n`).toString('base64');
+
+//       await axios.put('https://api.github.com/repos/Shantanu-CometChat/testingcommit/contents/README.md', {
+//         message: 'Updated README.md from Cloudsmith webhook',
+//         content: newContent,
+//         sha,
+//       }, {
+//         headers: {
+//           'Authorization': `token ${token}`,
+//         },
+//       });
+
+//       res.status(200).send('OK');
+//     } catch (error) {
+//       console.error('Error during GitHub operations', error);
+//       res.status(500).send('An error occurred');
+//     }
+//   });
+
+// module.exports = app;
